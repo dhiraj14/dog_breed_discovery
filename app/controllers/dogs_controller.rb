fd3eaf2df70e049ae2ae27c032752f00ab3a5565
DHIRAJ MISHRA <mdhiraj01@gmail.com>
2024-01-25 12:09:52 +0530

    Handle required params

--- app/controllers/dogs_controller.rb
+++ app/controllers/dogs_controller.rb
@@ -1,6 +1,18 @@
 class DogsController < ApplicationController
+  before_action :validate_breed_param
+
   def fetch_image
-    image_url = DogImageService.new(params[:breed]).fetch_random_image
+    image_url = DogImageService.new(breed_param).fetch_random_image
     render json: { image_url: image_url }
+  end
+
+  private
+
+  def breed_param
+    params[:breed]
+  end
+
+  def validate_breed_param
+    render json: { error: 'Breed parameter is required' }, status: :bad_request unless breed_param.present?
   end
 end
