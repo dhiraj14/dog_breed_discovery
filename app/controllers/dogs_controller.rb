dd9b1db2cc415bd902b4f8507db7cae09bd26d8d
DHIRAJ MISHRA <mdhiraj01@gmail.com>
2024-01-25 12:12:13 +0530

    Handle error in response from api

--- app/controllers/dogs_controller.rb
+++ app/controllers/dogs_controller.rb
@@ -1,9 +1,11 @@
 class DogsController < ApplicationController
-  before_action :validate_breed_param
+  before_action :validate_breed_param, only: :fetch_image
 
   def fetch_image
     image_url = DogImageService.new(breed_param).fetch_random_image
     render json: { image_url: image_url }
+  rescue DogImageService::FetchError => e
+    render json: { error: e.message }, status: :bad_gateway
   end
 
   private
--- app/services/dog_image_service.rb
+++ app/services/dog_image_service.rb
@@ -3,6 +3,8 @@ require 'net/http'
 class DogImageService
   API_BASE_URL = "https://dog.ceo/api/breed".freeze
 
+  class FetchError < StandardError; end
+
   def initialize(breed)
     @breed = breed
   end
@@ -10,6 +12,8 @@ class DogImageService
   def fetch_random_image
     response = Net::HTTP.get(URI("#{API_BASE_URL}/#{@breed}/images/random"))
     data = JSON.parse(response)
+
+    raise FetchError, 'Error fetching image' unless data['status'] == 'success'
     data['message']
   end
 end
